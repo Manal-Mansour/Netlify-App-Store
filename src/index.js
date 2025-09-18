e4f3e6d4d6d1aeba9159fdc1868640c83226ce21
Manal-Mansour <47292035+Manal-Mansour@users.noreply.github.com>
2025-09-18 11:19:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
